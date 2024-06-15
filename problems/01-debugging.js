6ffd8c598e8f25261842a68c310056b19454f24f
myrandathompson <myrandathompson@icloud.com>
2024-06-15 01:12:15 -0700

    questions 1 & 2

--- problems/01-debugging.js
+++ problems/01-debugging.js
@@ -4,13 +4,18 @@ Debugging
 Fix the bug in the code so that the example inputs produce the expected outputs.
 */
 
-function medianNum(sortedNums) {
-  if (sortedNums.length === 0) return null;
-  let midIdx = sortedNums.length / 4;
-  if (sortedNums.length % 2 !== 0) {
-    midIdx = (sortedNums.length - 1) / 2;
+function medianNum(sortedNums)  {
+  const length = sortedNums.length;
+  if (length === 0) {
+    return null;
   }
-  return sortedNums[midIdx];
+
+  const midIdx = Math.floor(length /2);
+  if (length % 2 === 2) {
+    return (sortedNums[midIdx - 1] + sortedNums[midIdx] / 2);
+  } else {
+    return sortedNums[midIdx];
+  }
 }
 
 // console.log(medianNum([1, 2, 3, 4, 5, 6])); //=> 4
